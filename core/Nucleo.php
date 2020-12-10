40db5ea8925ab65413802bc6db2e43a2220c2526
Jonathan <jonathanmendozalfredo@gmail.com>
2020-12-10 11:53:06 -0600

    CREACION DE UNA VERSION BETA PARA PAGINACION DE DATOS ADEMAS DE UN METODO QUE RETORNA LOS DATOS MEDIANTE UN QUERY PERSONALIZADO, FALTA DOCUMENTAR ESTOS ELEMENTOS

--- core/Nucleo.php
+++ core/Nucleo.php
@@ -1,14 +1,41 @@
 <?php
-require('Conexion.php');
+require_once('Conexion.php');
 class Nucleo
 {
+    //VARIABLE UTILIZADA PARA ESTABLECER LA CONEXION A LA BASE DE DATOS
     private $conexion;
+    //VARIABLE UTILIZADA PARA ALMACENAR EL QUERY PERSONALIZADO POR EL DESARROLLADOR
     private $queryPersonalizado;
+    //VARIABLE UTILIZADA PARA ESTABLECER LA TABLA EN LA BASE DE DATOS
+    //DONDE SE EJECUTARA ALGUNA OPCION
     private $tablaBase;
+    //VARIABLE UTILIZADA PARA DETERMINAR SI UNA CONSULTA DEBE SER ORDENADA
     private $ordenar;
+    //VARIABLE UTILIZADA PARA ALMACENAR EL TOTAL DE CAMPOS EN UNA TABLA DE LA BASE DE DATOS
     private $totalCampos;
+    //VARIABLE UTILIZADA PARA DETERMINAR SI EL DESARROLLADOR
+    //DESEA OBTENER EL ID DEL REGISTRO ALMACENADO EN LA BASE DE DATOS
+    //SOLO ES POSIBLE EN TABLAS CON PRIMARY KEY SERIAL O AUTOINCREMENTABLE
     private $regresarId = false;
+    //VARIABLE UTILIZADA PARA ALMACENAR TEMPORALMENTE EL ID DEL REGISTRO ALMACENADO
     private $id;
+    //VARIABLES PARA EL ENTORNO DE PAGINACION
+    //TOTAL DE REGISTROS A MOSTRAR POR PAGINA
+    private $porPagina = 10;
+    //MAXIMO DE ENLACES PARA PAGINAS A GENERAR
+    private $maximoEnlace = 4;
+
+    //ALMACENA LA QUERY NECESARIA PARA CALCULAR EL TOTAL DE REGISTROS DE LA PAGINACION
+    private $queryTotalRegistroPag;
+    //ALMACENA LA QUERY NECESARIA PARA EXTRAER LOS REGISTROS DE LA PAGINACION
+    private $queryExtractRegistroPag;
+    //UTILIZADA PARA IDENTIFICAR EL NUMERO DE PAGINA QUE EL USUARIO SOLICITA
+    private $numPagina;
+    //UTILIZADA PARA ALMACENAR EL TOTAL DE PAGINAS QUE SE OBTENDRAN
+    private $total_paginas;
+
+
+
     public function __construct()
     {
         $obj = new Conexion();
@@ -30,6 +57,10 @@ class Nucleo
     {
         return $this->obtenerData();
     }
+    public function getDatosParametros($campos)
+    {
+        return $this->obtenerDataParametros($campos);
+    }
     //SECCION DE ELIMINACION
     public function eliminarTodo()
     {
@@ -39,7 +70,11 @@ class Nucleo
     {
         return $this->eliminaPorQuery($campos);
     }
-
+    //SECCION DE PAGINACION
+    public function getDatosHtml(array $campos, array $botones, $identificador)
+    {
+        return $this->generarTablaHtml($campos, $botones, $identificador);
+    }
 
     private function nuevoRegistro($campos)
     {
@@ -67,9 +102,12 @@ class Nucleo
                     if ($resultado->rowCount() > 0) {
                         $this->id = $this->conexion->lastInsertId();
                         $resultado->closeCursor();
-                        return $this->regresarId ? $this->id : true;
+                        $respuesta = $this->regresarId ? $this->id : true;
+                        $this->limpiarEntorno();
+                        return $respuesta;
                     }
                     $resultado->closeCursor();
+                    $this->limpiarEntorno();
                     return false;
                 }
             }
@@ -105,8 +143,10 @@ class Nucleo
                 }
                 if ($resultado->execute()) {
                     $resultado->closeCursor();
+                    $this->limpiarEntorno();
                     return true;
                 }
+                $this->limpiarEntorno();
                 $resultado->closeCursor();
             }
             return false;
@@ -194,6 +234,7 @@ class Nucleo
                 $resultado->execute();
                 $datos = $resultado->fetchAll(PDO::FETCH_ASSOC);
                 $resultado->closeCursor();
+                $this->limpiarEntorno();
                 if ($datos) {
                     return $datos;
                 }
@@ -212,8 +253,10 @@ class Nucleo
                 $resultado = $this->conexion->prepare($consulta);
                 if ($resultado->execute() && $resultado->rowCount() > 0) {
                     $resultado->closeCursor();
+                    $this->limpiarEntorno();
                     return true;
                 }
+                $this->limpiarEntorno();
                 $resultado->closeCursor();
             }
             return false;
@@ -230,8 +273,10 @@ class Nucleo
                 $resultado = $this->conexion->prepare($consulta);
                 if ($resultado->execute($campos) && $resultado->rowCount() > 0) {
                     $resultado->closeCursor();
+                    $this->limpiarEntorno();
                     return true;
                 }
+                $this->limpiarEntorno();
                 $resultado->closeCursor();
             }
             return false;
@@ -295,7 +340,209 @@ class Nucleo
             die("El error de la conexión fue: " . $e->getMessage());
         }
     }
+    //OBTIENE TODOS LOS REGISTROS DE LA TABLA INDICADA CON PARAMETROS
+    private function obtenerDataParametros($campos)
+    {
+        try {
+            if (!empty($this->tablaBase)) {
+                $consulta = "";
+                if (!empty($this->queryPersonalizado)) {
+                    $consulta = $this->queryPersonalizado;
+                    $resultado = $this->conexion->prepare($consulta);
+                    $resultado->execute($campos);
+                    $datos = $resultado->fetchAll(PDO::FETCH_ASSOC);
+                    $resultado->closeCursor();
+                    $this->limpiarEntorno();
+                    if ($datos) {
+                        return $datos;
+                    }
+                }
+            }
+            return null;
+        } catch (PDOException $e) {
+            die("El error de la conexión fue: " . $e->getMessage());
+        }
+    }
 
+
+    //SECCION DE LA PAGINACION
+    //LISTAR DATOS
+    public function generarTablaHtml(array $campos, $botones, $identificador)
+    {
+        try {
+            $empezarDesde = $this->iniciarDesde($this->numPagina);
+            if (GESTOR == 1) {
+                $this->queryExtractRegistroPag = $this->queryExtractRegistroPag  . " LIMIT " .  $this->porPagina . " OFFSET "  . $empezarDesde;
+            } else {
+                $this->queryExtractRegistroPag = $this->queryExtractRegistroPag  . " LIMIT " . $empezarDesde  . ", "  . $this->porPagina;
+            }
+            $totalRegistros = $this->numeroRegistros();
+            $this->total_paginas = $this->numeroPaginas($totalRegistros);
+            $datos = $this->consultarDatos();
+            $tabla = "";
+            //OBTIENE EL HTML A MOSTRAR
+            foreach ($datos as $objeto) {
+                $fila = "<tr class='text-center' >";
+                foreach ($campos as $valor) {
+                    $fila = $fila . "<td>{$objeto[$valor]}</td>";
+                    $fila = $fila . "<td>";
+                    foreach ($botones as $boton => $tipo)
+                        $fila = $fila . "<button obj$boton='{$objeto[$identificador]}'  type='button'  class='$boton btn btn-primary btn-sm'>
+                                        <i class='fas fa-$tipo'></i></button>";
+                }
+                $fila = $fila . "</td>";
+                $fila = $fila . "</tr>\n";
+                $tabla = $tabla . $fila;
+            }
+            $paginador = $this->enlaces();
+
+            //ARRAY QUE SE ENVIARA A JS
+            $data = array();
+            $data["totalRegistros"] = $totalRegistros;
+            $data["tabla"] = ($totalRegistros > 0) ? $tabla : "<td class='text-center text-info'>No hay registros para mostrar</td><td></td><td></td>";
+            $data["paginador"] = $paginador;
+            $data["totalPagina"] = $this->total_paginas;
+            $data["paginaActual"] = ($totalRegistros > 0) ? $this->numPagina : 0;
+            $data["desde"] = ($totalRegistros > 0) ? $empezarDesde + 1 : 0;
+            if (($empezarDesde + $this->porPagina) > $totalRegistros) {
+                $data["hasta"] = $totalRegistros;
+            } else {
+                $data["hasta"] = $empezarDesde + $this->porPagina;
+            }
+            return $data;
+        } catch (PDOException $e) {
+            die('Ocurrio un error: ' . $e->getMessage());
+        }
+    }
+    //CALCULA DESDE DONDE SE EMPEZARA A TRAER LOS REGISTROS DE LA BASE DE DATOS
+    private function iniciarDesde()
+    {
+        return ($this->numPagina - 1) * $this->porPagina;
+    }
+    //CALCULA EL NUMERO DE PAGINAS QUE SE VAN A MOSTRAR
+    private function numeroPaginas($totalRegistros)
+    {
+        return ceil($totalRegistros / $this->porPagina);
+    }
+
+    //OBTIENE EL NUMERO DE REGISTROS EN LA BASE DE DATOS SEGUN QUERY
+    private  function numeroRegistros()
+    {
+        try {
+            $totalRegistros = 0;
+            $resultado = $this->conexion->prepare($this->queryTotalRegistroPag);
+            $resultado->execute();
+            $totalRegistros = $resultado->fetchAll(PDO::FETCH_ASSOC);
+            $resultado->closeCursor();
+            $totalRegistros = $totalRegistros[0]["total"];
+            settype($totalRegistros, 'int');
+            return $totalRegistros;
+        } catch (PDOException $e) {
+            die("Ocurrio un error en la consulta: " . $e->getMessage());
+        }
+    }
+    //CONSULTA LOS DATOS
+    private function consultarDatos()
+    {
+        try {
+            $resultado = $this->conexion->prepare($this->queryExtractRegistroPag);
+            $resultado->execute();
+            $datos = $resultado->fetchAll(PDO::FETCH_ASSOC);
+            $resultado->closeCursor();
+            return $datos;
+        } catch (PDOException $e) {
+            die("Ocurrio un error en la consulta: " . $e->getMessage());
+        }
+    }
+    //GENERA LOS ENLACES DE LA PAGINACION
+    private function enlaces()
+    {
+        $paginador = "";
+        $indice = $this->numPagina;
+        $siguiente = 0;
+        if ($this->total_paginas > $this->maximoEnlace) {
+            if ($this->numPagina == 1) {
+                $paginador = $paginador .  "<li  class='page-item disabled'>
+                                         <a class='page-link' href='#'>Anterior</a></li>";
+            } else {
+                $paginador = $paginador .  "<li pag = '" . ($this->numPagina - 1) . "' class='siguiente page-item'>
+                                         <a class='page-link' href='#'>Anterior</a></li>";
+            }
+            if ($this->numPagina == 1) {
+                $paginador = $paginador . "<li   class='page-item active'><a class='page-link disable' href='#'>1</a></li>";
+            } else {
+                $paginador = $paginador . "<li  pag = '1' class='pagina page-item'><a class='page-link' href='#'>1</a></li>";
+            }
+            if ((($this->total_paginas) - $this->numPagina < $this->maximoEnlace) && $this->numPagina >= $this->maximoEnlace) {
+                $inicio = (($this->total_paginas) - $this->maximoEnlace);
+                while ($this->maximoEnlace > 0) {
+                    if ($inicio > $this->total_paginas) {
+                        break;
+                    } else {
+                        if ($inicio != 1 && $inicio != $this->total_paginas) {
+                            if ($inicio == $this->numPagina) {
+                                $paginador = $paginador . "<li   class='page-item active'><a class='page-link disable' href='#'>{$inicio}</a></li>";
+                            } else {
+                                $paginador = $paginador . "<li  pag = '{$inicio}' class='pagina page-item'><a class='page-link' href='#'>{$inicio}</a></li>";
+                            }
+                            $this->maximoEnlace--;
+                            $indice++;
+                        }
+                    }
+                    $inicio++;
+                }
+            } else {
+                for ($i = $this->numPagina; $i <= $this->total_paginas; $i++) {
+                    if ($this->maximoEnlace == 0) {
+                        break;
+                    }
+                    if ($i != 1 && $i != $this->total_paginas) {
+                        if ($i == $this->numPagina) {
+                            $paginador = $paginador . "<li   class='page-item active'><a class='page-link disable' href='#'>{$i}</a></li>";
+                        } else {
+                            $paginador = $paginador . "<li  pag = '{$i}' class='pagina page-item'><a class='page-link' href='#'>{$i}</a></li>";
+                        }
+                        $this->maximoEnlace--;
+                    }
+                    $indice++;
+                }
+            }
+            if ($this->numPagina == $this->total_paginas) {
+                $paginador = $paginador . "<li   class='page-item active'><a class='page-link disable' href='#'>$this->total_paginas</a></li>";
+            } else {
+                $paginador = $paginador . "<li  pag = '$this->total_paginas' class='pagina page-item'><a class='page-link' href='#'>$this->total_paginas</a></li>";
+            }
+            if ($indice > $this->total_paginas) {
+                $paginador = $paginador .  "<li  class='page-item disabled'>
+                                             <a class='page-link' href='#'>Siguiente</a></li>";
+            } else {
+                $paginador = $paginador .  "<li pag = '" . ($indice) . "' class='siguiente page-item'>
+                                             <a class='page-link' href='#'>Siguiente</a></li>";
+            }
+        } else {
+            $paginador = $paginador .  "<li  class='page-item disabled'>
+                                         <a class='page-link' href='#'>Anterior</a></li>";
+            for ($i = 1; $i <= $this->total_paginas; $i++) {
+                if ($i == $this->numPagina) {
+                    $paginador = $paginador . "<li   class='page-item active'><a class='page-link disable' href='#'>{$i}</a></li>";
+                } else {
+                    $paginador = $paginador . "<li  pag = '{$i}' class='pagina page-item'><a class='page-link' href='#'>{$i}</a></li>";
+                }
+            }
+            $paginador = $paginador .  "<li  class='page-item disabled'>
+                                         <a class='page-link' href='#'>Siguiente</a></li>";
+        }
+        return $paginador;
+    }
+    private function limpiarEntorno()
+    {
+
+        $this->queryPersonalizado = "";
+        $this->ordenar = "";
+        $this->totalCampos = 0;
+        $this->regresarId = false;
+        $this->id = "";
+    }
     /**
      * Get the value of queryPersonalizado
      */
@@ -371,6 +618,94 @@ class Nucleo
     public function setRegresarId($regresarId)
     {
         $this->regresarId = $regresarId;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of porPagina
+     */
+    public function getPorPagina()
+    {
+        return $this->porPagina;
+    }
+
+    /**
+     * Set the value of porPagina
+     *
+     * @return  self
+     */
+    public function setPorPagina($porPagina)
+    {
+        $this->porPagina = $porPagina;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of maximoEnlace
+     */
+    public function getMaximoEnlace()
+    {
+        return $this->maximoEnlace;
+    }
+
+    /**
+     * Get the value of queryTotalRegistroPag
+     */
+    public function getQueryTotalRegistroPag()
+    {
+        return $this->queryTotalRegistroPag;
+    }
+
+    /**
+     * Set the value of queryTotalRegistroPag
+     *
+     * @return  self
+     */
+    public function setQueryTotalRegistroPag($queryTotalRegistroPag)
+    {
+        $this->queryTotalRegistroPag = $queryTotalRegistroPag;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of queryExtractRegistroPag
+     */
+    public function getQueryExtractRegistroPag()
+    {
+        return $this->queryExtractRegistroPag;
+    }
+
+    /**
+     * Set the value of queryExtractRegistroPag
+     *
+     * @return  self
+     */
+    public function setQueryExtractRegistroPag($queryExtractRegistroPag)
+    {
+        $this->queryExtractRegistroPag = $queryExtractRegistroPag;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of numPagina
+     */
+    public function getNumPagina()
+    {
+        return $this->numPagina;
+    }
+
+    /**
+     * Set the value of numPagina
+     *
+     * @return  self
+     */
+    public function setNumPagina($numPagina)
+    {
+        $this->numPagina = $numPagina;
 
         return $this;
     }
